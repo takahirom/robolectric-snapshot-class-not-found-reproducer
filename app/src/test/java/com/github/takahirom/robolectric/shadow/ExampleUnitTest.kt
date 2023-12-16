2bd79883719f77735136f6e3f9588e1915c56b8d
takahirom <takam.dev@gmail.com>
2023-12-16 14:15:01 +0900

    Add some context

--- app/src/test/java/com/github/takahirom/robolectric/shadow/ExampleUnitTest.kt
+++ app/src/test/java/com/github/takahirom/robolectric/shadow/ExampleUnitTest.kt
@@ -10,6 +10,7 @@ import org.junit.Test
 import org.junit.Assert.*
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 import org.robolectric.annotation.GraphicsMode
 
 /**
@@ -18,10 +19,16 @@ import org.robolectric.annotation.GraphicsMode
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [33])
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
 class ExampleUnitTest {
     @Test
     fun test() {
+        // print java version
+        println("System.getProperty(\"java.version\"):" + System.getProperty("java.version"))
+        // java home
+        println("System.getProperty(\"java.home\"):" + System.getProperty("java.home"))
+
         System.setProperty("robolectric.screenshot.hwrdr.native", "true")
         val activityScenario = launch(MainActivity::class.java)
         activityScenario.onActivity { activity ->
